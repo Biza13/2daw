9b8176c2e9918ab5fa73800814d298f3609257ac
Biza_13 <serrano_0_1@hotmail.com>
2024-10-24 18:53:22 +0200

    Ejercicio arrays sergio terminado

--- Entorno cliente/U3/Ejercicio 2/funciones.js
+++ Entorno cliente/U3/Ejercicio 2/funciones.js
@@ -81,9 +81,13 @@ function compararArrays(arr1, arr2){
 }
 
 function contenido(cad){
+    let cadMin = cad.toLowerCase();
+    let esta = false;
     let arr = [];
     //con .textContent coge el texto contenido de las etiquetas
     let parrafos = document.getElementById("p").textContent;
+
+    //separamos los parrafos por los espacios, haciendo que cada palabra sea un elemento del array
     arr = parrafos.split(" ")
 
     /*1)El metodo filter crea un nuevo array con los elementos que cumplan la condiccion especificada
@@ -94,5 +98,76 @@ function contenido(cad){
       si por lo contrari SI esta vacia devolvera false
       4)por lo tanto filter devolvera un nuevo array solo con los elementos que devuelvan true*/
     arr = arr.filter(element => element.trim() !== "");
+
+    /*Recorremos es array para eliminar los puntos las comas y los retornos de carro
+    puesto a que replace no nodifica el array habra que asignarle a la palabra a modificar
+    el mismo valor pero sin puntos, comas o retornos de carro y asi SI modificamos las strings del array
+    /[.,\n]/g    Las barras indican el comienzo y el final de una expresion regular, es decir la expresión
+    buscara lo que haya dentro de las barras y la g es para que lo busque hasta el final,
+    si no la pones buscara hasta encontrar la primera palabra con una coma, punto o retorno y dejara de buscar en las demas
+    y por ultimo, convertimos a minusculas todo el texto*/
+    for(let i = 0; i<arr.length; i++){
+        arr[i] = arr[i].replace(/[.,\n]/g, "").toLowerCase();
+        
+    } 
+
+    if (arr.includes(cadMin)){
+        esta = true;
+    }
+    return esta;
+}
+
+function ordenarBurbuja(arr){
+    let long = arr.length;
+    let guar;
+    for(let x = 0; x<(long-1); x++){
+        for(let i = 0; i<((long-1)-x); i++){
+            if (arr[i] > arr[i+1]){
+                guar = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = guar;
+            }
+        }
+    }
+    //eliminando los duplicados
+    arr = [...new Set(arr)]
     return arr;
+}
+
+function ordenarNombres(cad){
+    let arr = cad.split(",");
+    for(let i = 0; i<arr.length; i++){
+        arr[i] = arr[i].trim().toLowerCase();
+        /*Para poner la primera en mayusculas
+        1)cogemos de arr[i].charAt(0) el primer caracter y lo pasamos a mayusculas con toUpperCase()
+        2)Al primer caracter le concatenamos el resto de la palabra con el slice(1) desde la posicion 1
+        que sera la segunda letra*/
+        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+    let cadena;
+    arr.sort();
+
+    //con esto separamos el array en coma y espacio lo convierte a cadena
+    //tambien lo hace el toString() y lo separa por comas pero sin espacio
+    cadena = arr.join(", ");
+    return cadena;
+}
+
+function infoCadena(cad){
+    let arr = cad.split(" ");
+    let palabras = arr.length;
+    let primera = arr[0];
+    let ultima = arr[palabras-1];
+    let inverso = [...arr].reverse();
+    /*el map devuelve un array con la modificacion que tu le hagas a cada elemento
+    es decir, cada elemento se va a llamar palabra, pues a palabra le voy a aplicar el toLowerCase()
+    y cuando ya se lo haya aplicado a todos le hago el sort()*/
+    let ordenAZ = [...arr].map(palabra => palabra.toLowerCase()).sort();
+    let ordenZA = [...ordenAZ].reverse();
+    return "Número de palabras: " + palabras + "<br>" +
+    "Primera palabra: " + primera + "<br>" +
+    "Última palabra: " + ultima + "<br>" +
+    "Orden inverso: " + inverso + "<br>" +
+    "Orden de la A a la Z: " + ordenAZ + "<br>" +
+    "Orden de la Z a la A: " + ordenZA;
 }
