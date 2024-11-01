2ea052fabdce28ab497b97661736843c80a9cbd5
Biza_13 <serrano_0_1@hotmail.com>
2024-11-01 23:24:30 +0100

    venga va que ya si

--- jsdoc/src/funciones.js
+++ jsdoc/src/funciones.js
@@ -28,9 +28,9 @@ function squareAndSum(){
 
 /**
  * la funcion "suma" takes two parameters, adds them together, and returns the result.
- * @param a - The function `suma(a, b)` takes two parameters, `a` and `b`, and returns their sum when
+ * @param {number} a - The function `suma(a, b)` takes two parameters, `a` and `b`, and returns their sum when
  * the function is called.
- * @param b - The parameter `b` in the `suma` function is the second number that will be added to the
+ * @param {number} b - The parameter `b` in the `suma` function is the second number that will be added to the
  * first number `a`.
  * @returns The function `suma` returns the sum of the two input parameters `a` and `b`.
  */
