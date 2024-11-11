ff15b73c47209bbb6315e451c95f2ef9d6fa787d
Biza_13 <serrano_0_1@hotmail.com>
2024-11-11 17:19:14 +0100

    Ejercicios sergio DOM

--- Entorno cliente/U5/Ej1/main.js
+++ Entorno cliente/U5/Ej1/main.js
@@ -1,27 +1,10 @@
-let enlaces = document.getElementsByTagName("a");
-console.log(enlaces);
-console.log(enlaces.length);
+import { arrArticulos } from "./modulos/articulos.mjs";
 
-let penultimo = enlaces[enlaces.length-2];
-let atributo = penultimo.getAttribute("href");
-console.log(atributo);
-
-console.log(penultimo.href);
-
-let cont = 0;
-for(let i = 0; i<enlaces.length; i++){
-    let a = enlaces[i];
-    if (a.href.includes("www.google.com")){
-        cont++;
-    }
-}
-
-console.log("Enlaces con href a google " + cont)
-
-let ps = document.getElementsByTagName("p");
-
-let ultimoP = ps[ps.length-1];
-
-let as = ultimoP.getElementsByTagName("a");
-
-console.log("Cantidad de enlaces del ultimo p: " + as.length);
+//Funcion Main
+(function Main() {
+  console.log("Mostrando artículos");
+  console.log(arrArticulos);
+  arrArticulos.forEach((articulo) => {
+    console.log(articulo);
+  });
+})();
