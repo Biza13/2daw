7f69accc0cdc8632625a4145b20816deacd0d800
Biza_13 <serrano_0_1@hotmail.com>
2024-11-30 14:34:18 +0100

    diseño propio ana

---
+++ Desarrollo de Interfaces/Tema 5 transiciones y transformaciones/Diseño propio/Pagina/funcion.js
@@ -0,0 +1,12 @@
+function sonido(){
+    let sonido = new Audio("./Pagina/recursos/mugido.mp3");
+    sonido.play();
+}
+
+/**
+*Funcion que reproduce un sonido mp3
+*/
+function sonido2(){
+    let sonido = new Audio("./recursos/mugido.mp3");
+    sonido.play();
+}
