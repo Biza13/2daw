3c895052efc9d0a4626c09b11b2ae883d2697691
Biza_13 <serrano_0_1@hotmail.com>
2025-02-25 11:51:42 +0100

    react native

--- Entorno cliente/React/reactNative/navegacion/app/(tabs)/index.tsx
+++ Entorno cliente/React/reactNative/navegacion/app/(tabs)/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Prinsipal</Text>
+      <Text>Principal</Text>
       <Link href="/detalles">
         Detalles Vista
       </Link>
